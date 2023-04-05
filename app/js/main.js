247a488e7c48f700156ae28df9659cca8b98473e
lev <lleevv2020@gmail.com>
2023-04-05 18:40:42 +0400

    add

---
+++ app/js/main.js
@@ -0,0 +1,3 @@
+const btn = document.querySelector('.box2'); 
+const btn3 = document.querySelector('.box3'); 
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,77 @@
-"use strict"
-
+//"use strict"
 const {
   src,
-  dest
+  dest,
+  watch,
+  parallel,
+  series // последовательне вызов
 } = require('gulp');
-const scss = require('sass')(require('gulp-sass'));
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
+
+
+function scripts(){
+  return src([
+    // swiper 
+    'node_modules/swiper/swiper-bundle.js',
+    'app/js/*.js',
+    '!app/js/main.min.js'
+  ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js/'))
+    .pipe(browserSync.stream())
+}
 
 function styles(){
   return src('app/scss/style.scss')
-    .pipe(scss())
+    .pipe(autoprefixer({
+      cascade: false,
+      grid: true,
+      overrideBrowserslist: ["last 5 versions"]
+    }))
+    .pipe(concat('style.min.css'))
+    .pipe(scss({outputStyle: 'compressed'}))
     .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
+}
+function watching(){
+  watch(['app/scss/style.scss'], styles)
+  watch(['app/js/main.js'], scripts)
+  watch(['app/**/*.html']).on('change', browserSync.reload);
 }
 
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: `app/`
+    },
+  });
+}
+
+function building() {
+  return src([
+    'app/css/style.min.css',
+    'app/js/main.min.js',
+    'app/**/*.html'
+  ], {base : 'app'})
+  .pipe(dest('dist'))
+}
+
+function cleanDist(){
+  return src('dist')
+    .pipe(clean())
+}
+
+
 exports.styles = styles;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.build = series(cleanDist, building);
+exports.default = parallel(styles, scripts, browsersync, watching);
